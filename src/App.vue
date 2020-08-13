617b3dd73234a043834cd6faa358aea3fc378dbf
renandaam <renandamelatiputri999@gmail.com>
2020-08-13 17:17:47 +0700

    Memperbarui tampilan

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div >
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
 
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
@@ -19,7 +19,7 @@
       </li>
     </ul>
     <form class="form-inline my-2 my-lg-0">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Join Pemateri</button>
+      <a href="https://www.lawgo.id" ><button>Join Pemateri</button></a>
     </form>
  
   </div>
@@ -27,15 +27,190 @@
     <HelloWorld msg=""/>
     <h1> Pelatihan </h1>
     <h2> Silahkan cari pelatihan,workshop,atau seminar yang anda minati</h2>
+    <form>
     <input type="text" v-model="search" placeholder="Cari pelatihan,workshop,seminar" />
     
+    </form>
+    <h3></h3>
     <img alt="Vue logo" src="./assets/Bitmap.png">
      <img alt="Vue logo" src="./assets/Bitmap (1).png">
       <img alt="Vue logo" src="./assets/Bitmap (2).png">
-
+<div class="container">
+  <div class="row">
+    <div class="col-sm">
+      Pelatihan Profesi
+      Advokat.
+    <br>
+     <span class="material-icons " style="color :yellow;">star_rate</span>
+     <span class="material-icons " style="color :yellow;">star_rate</span>
+     <span class="material-icons " style="color :yellow;">star_rate</span>
+     <span class="material-icons " style="color :yellow;">star_rate</span>
+     
+      <br>
+      <span class="material-icons">location_on</span>
+      <span>BSD City,Banten</span>
+      <br>
+      <span class="material-icons">calendar_today</span>
+      <span>Sabtu,12 September 2019</span>
+      <br>
+      <a href="https://www.lawgo.id" ><button>Booking Now</button></a>
+      <br>
+    </div>
+    <div class="col-sm">
+      Seminar Hukum di Era 4.0
+      <br>
+      <span class="material-icons " style="color :yellow;">star_rate</span>
+     <span class="material-icons " style="color :yellow;">star_rate</span>
+     <span class="material-icons " style="color :yellow;">star_rate</span>
+     <span class="material-icons " style="color :yellow;">star_rate</span>
+     <br>
+      <span class="material-icons">location_on</span>
+      <span>Mega Kuningan,Jakarta Selatan</span>
+      <br>
+      <span class="material-icons">calendar_today</span>
+      <span>Sabtu,12 September 2019</span>
+      <br>
+      <a href="https://www.lawgo.id" ><button>Booking Now</button></a>
+      <br>
+    </div>
+    <div class="col-sm">
+      Seminar Pendidikan Profesi
+      <br>
+     <span class="material-icons " style="color :yellow;">star_rate</span>
+     <span class="material-icons " style="color :yellow;">star_rate</span>
+     <span class="material-icons " style="color :yellow;">star_rate</span>
+     <span class="material-icons " style="color :yellow;">star_rate</span>
+     <br>
+      <span class="material-icons">location_on</span>
+      <span>Mega Kuningan,Jakarta Selatan</span>
+      <br>
+      <span class="material-icons">calendar_today</span>
+      <span>Sabtu,12 September 2019</span>
+      <br>
+      <a href="https://www.lawgo.id" ><button>Booking Now</button></a>
+      <br>
+    </div>
+  </div>
+</div>
+<img alt="Vue logo" src="./assets/Bitmap (3).png">
+     <img alt="Vue logo" src="./assets/Bitmap (4).png">
+      <img alt="Vue logo" src="./assets/Bitmap (5).png">
+      <div class="container">
+  <div class="row">
+    <div class="col-sm">
+      Architecture Training for beginner
+      <br>
+     <span class="material-icons " style="color :yellow;">star_rate</span>
+     <span class="material-icons " style="color :yellow;">star_rate</span>
+     <span class="material-icons " style="color :yellow;">star_rate</span>
+     <span class="material-icons " style="color :yellow;">star_rate</span>
+     <br>
+      <span class="material-icons">location_on</span>
+      <span>BSD City,Banten</span>
+      <br>
+      <span class="material-icons">calendar_today</span>
+      <span>Sabtu,12 September 2019</span>
+      <br>
+      <a href="https://www.lawgo.id" ><button>Booking Now</button></a>
+      <br>
+    </div>
+    <div class="col-sm">
+      Seminar UX Design Principle
+      <br>
+      <span class="material-icons " style="color :yellow;">star_rate</span>
+     <span class="material-icons " style="color :yellow;">star_rate</span>
+     <span class="material-icons " style="color :yellow;">star_rate</span>
+     <span class="material-icons " style="color :yellow;">star_rate</span>
+     <br>
+      <span class="material-icons">location_on</span>
+      <span>Mega Kuningan,Jakarta Selatan</span>
+      <br>
+      <span class="material-icons">calendar_today</span>
+      <span>Sabtu,12 September 2019</span>
+      <br>
+      <a href="https://www.lawgo.id" ><button>Booking Now</button></a>
+      <br>
+    </div>
+    <div class="col-sm">
+      Workshop Graphic Designer
+      <br>
+      <span class="material-icons " style="color :yellow;">star_rate</span>
+     <span class="material-icons " style="color :yellow;">star_rate</span>
+     <span class="material-icons " style="color :yellow;">star_rate</span>
+     <span class="material-icons " style="color :yellow;">star_rate</span>
+     <br>
+      <span class="material-icons">location_on</span>
+      <span>Mega Kuningan,Jakarta Selatan</span>
+      <br>
+      <span class="material-icons">calendar_today</span>
+      <span>Sabtu,12 September 2019</span>
+      <br>
+      <a href="https://www.lawgo.id" ><button>Booking Now</button></a>
+      <br>
+    </div>
+  </div>
+</div>
+<img alt="Vue logo" src="./assets/Bitmap (6).png">
+     <img alt="Vue logo" src="./assets/Bitmap (7).png">
+      <img alt="Vue logo" src="./assets/Bitmap (8).png">
+      <div class="container">
+  <div class="row">
+    <div class="col-sm">
+      Pelatihan Illustrator menggunakan Procreate
+      <br>
+     <span class="material-icons " style="color :yellow;">star_rate</span>
+     <span class="material-icons " style="color :yellow;">star_rate</span>
+     <span class="material-icons " style="color :yellow;">star_rate</span>
+     <span class="material-icons " style="color :yellow;">star_rate</span>
+     <br>
+      <span class="material-icons">location_on</span>
+      <span>BSD City,Banten</span>
+      <br>
+      <span class="material-icons">calendar_today</span>
+      <span>Sabtu,12 September 2019</span>
+      <br>
+      <a href="https://www.lawgo.id" ><button>Booking Now</button></a>
+      <br>
+    </div>
+    <div class="col-sm">
+      Basic Programming with C++
+      <br>
+      <span class="material-icons " style="color :yellow;">star_rate</span>
+     <span class="material-icons " style="color :yellow;">star_rate</span>
+     <span class="material-icons " style="color :yellow;">star_rate</span>
+     <span class="material-icons " style="color :yellow;">star_rate</span>
+     <br>
+      <span class="material-icons">location_on</span>
+      <span>Mega Kuningan,Jakarta Selatan</span>
+      <br>
+      <span class="material-icons">calendar_today</span>
+      <span>Sabtu,12 September 2019</span>
+      <br>
+      <a href="https://www.lawgo.id" ><button>Booking Now</button></a>
+      <br>
+    </div>
+    <div class="col-sm">
+      Product Design with Blender
+      <br>
+      <span class="material-icons " style="color :yellow;">star_rate</span>
+     <span class="material-icons " style="color :yellow;">star_rate</span>
+     <span class="material-icons " style="color :yellow;">star_rate</span>
+     <span class="material-icons " style="color :yellow;">star_rate</span>
+     <br>
+      <span class="material-icons">location_on</span>
+      <span>Mega Kuningan,Jakarta Selatan</span>
+      <br>
+      <span class="material-icons">calendar_today</span>
+      <span>Sabtu,12 September 2019</span>
+      <br>
+      <a href="https://www.lawgo.id" ><button>Booking Now</button></a>
+      <br>
+    </div>
+  </div>
+</div>
   </div>
   
-  
+
 </template>
 
 <script>
@@ -59,11 +234,11 @@ body{
 }
 h1 {
   text-align: center;
-  color: #fff;
+
   margin-top: 20px;
   font-family: Avenir;
   font-style: normal;
-  font-weight: 900;
+  font-weight: 500;
   font-size: 53px;
 
 
@@ -71,8 +246,9 @@ h1 {
 h2 {
     font-family: sans-serif; 
   text-align: center;
-  color: #fff;
-  font-size: 16px;
+
+  font-size: 9px;
+  
 
 }
 input {
@@ -80,38 +256,44 @@ input {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  
+  
+  
 }
 
-header{
+div {
+  background-color: #0A93FF;
 
-  width: 100vw;
-  padding:10px; 
 }
 nav{
-  background:#0A93FF;
-  padding: 20px 0;
+
+  padding: 0;
   font-size: 12px;
-  margin-bottom: 40px;
   font-family: HelveticaNeue;
 }
 
 a{
-  color:#fff;
+
   text-decoration:none;
-  padding:6px 8px;
   border-radius:10px;
+  color:blue;
+  
+  
 }
 li{
   display: inline-block;
   margin: 0 10px;
+  
 }
 ul{
   list-style-type:none;
   margin:0;
+  
 }
 button{
 
   float: right;
+
 }
 img{
   width:350px;
@@ -120,7 +302,4 @@ img{
   
 }
 
-input{
-  
-}
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-
+  
   </div>
+  
 </template>
 
 <script>
